d4beae0d625150098041ec1aedb8f785e5893b0a
yunaj9910-alt <yunaj9910@gmail.com>
2026-01-05 09:12:18 +0900

    codecata_0105

---
+++ codecata_251222/codecata_0105.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    if (num % 2 == 0)
+    {
+        answer = "Even";
+    }
+    else
+    {
+        answer = "Odd";
+    }
+    return answer;
+}
+
+int main()
+{
+    cout << solution(3) << endl;
+    cout << solution(4) << endl;
+}
