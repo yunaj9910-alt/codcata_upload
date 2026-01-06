e020f5fe37f4648611ea7b0396c0b7fba6862092
yunaj9910-alt <yunaj9910@gmail.com>
2026-01-06 09:29:04 +0900

    codecata_0106

---
+++ codecata_251222/codecata_0106.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        answer += arr[i];
+    }
+    return answer/arr.size();
+}
+
+int main()
+{
+    vector <int> arr1 = { 1, 2, 3, 4 };
+    double answer = solution(arr1);
+    cout << answer << endl;
+
+    vector<int>arr2 = { 5, 5 };
+    cout << solution(arr2)<<endl;
+  
+}
