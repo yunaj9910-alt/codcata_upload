48dbc67579432ebb3ba482da3bb78c67dc204324
yunaj9910-alt <yunaj9910@gmail.com>
2026-01-08 09:22:58 +0900

    codecata_0108

---
+++ codecata_251222/codecata_0108.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 1; i<=n; i++)
+    {
+        if (n % i == 0)
+        {
+            answer += i;
+        }
+        
+    }
+    return answer;
+}
+
+int main()
+{
+    cout << solution(12) << endl;
+    cout << solution(5) << endl;
+}
