8f52466bd1dfc9fcd9002de0eb31740c9d2302bb
yunaj9910-alt <yunaj9910@gmail.com>
2026-01-09 09:17:47 +0900

    Create codecata_0109.cpp

---
+++ codecata_251222/codecata_0109.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+  
+    for (int x = 1; x < n; x++)
+        {
+         if (n % x == 1)
+          {
+             return x;
+          }
+                
+    }
+        
+}
+
+int main()
+{
+    cout << solution(10) << endl;
+    cout << solution(12) << endl;
+}
