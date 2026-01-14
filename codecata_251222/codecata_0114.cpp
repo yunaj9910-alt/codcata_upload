237d7b0a5bb234ef3ca8cdb7227a42e4792644c4
yunaj9910-alt <yunaj9910@gmail.com>
2026-01-14 09:36:12 +0900

    codecata_0114

---
+++ codecata_251222/codecata_0114.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    bool isNegative = false;
+
+    if (s[0] == '-')
+    {
+        isNegative = true;
+    }
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            answer = answer * 10 + (s[i] - '0');
+        }
+    }
+
+    if (isNegative)
+    {
+        answer *= -1;
+    }
+   
+    return answer;
+}
+
+int main()
+{
+    cout << solution("1234") << endl;
+    cout << solution("-1234") << endl;
+}
