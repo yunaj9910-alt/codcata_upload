c956af4b57591f1ea7e3c3480443602794c354e1
yunaj9910-alt <yunaj9910@gmail.com>
2026-01-15 09:34:00 +0900

    codecata_0115

---
+++ codecata_251222/codecata_0115.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long x = static_cast<long long>(sqrt(n));
+
+    if (x * x == n)
+    {
+        return(x + 1) * (x + 1);
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int main()
+{
+    cout << solution(121) << endl;
+    cout << solution(3) << endl;
+}
