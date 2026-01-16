4ee6167926343fe6232886044dc980539dff4afa
yunaj9910-alt <yunaj9910@gmail.com>
2026-01-16 09:32:51 +0900

    codecata_0116

---
+++ codecata_251222/codecata_0116.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm> // sort를 사용하기 위해 추가
+#include <functional> // greater를 사용하기 위해 추가
+
+using namespace std;
+
+long long solution(long long n) {
+    string s = to_string(n);
+    sort(s.begin(), s.end(), greater<char>());
+    long long answer = stoll(s);
+    return answer;
+}
+
+int main()
+{
+    cout << solution(118372) << endl;
+}
