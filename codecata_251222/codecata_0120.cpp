ad52a7244bd5067052002ee2cbed70772fab6f6f
yunaj9910-alt <yunaj9910@gmail.com>
2026-01-20 09:28:22 +0900

    codecata_0120

---
+++ codecata_251222/codecata_0120.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    int start = min(a, b);
+    int end = max(a, b);
+
+    long long answer = 0;
+
+    for (int i = start; i <= end; ++i)
+    {
+        answer += i;
+    }
+    return answer;
+}
+
+int main()
+{
+    cout << solution(3, 5) << endl;
+    cout << solution(3, 3) << endl;
+    cout << solution(5, 3) << endl;
+}
