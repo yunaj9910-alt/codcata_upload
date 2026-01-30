e6038edad524deaebb0455d0be8783d11f2351a3
yunaj9910-alt <yunaj9910@gmail.com>
2026-01-30 09:19:46 +0900

    codecata_0130

---
+++ codecata_251222/codecata_0130.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int len = s.length();
+    int mid = len / 2;
+
+    if (len % 2 == 0)
+    {
+        return s.substr(mid - 1, 2);
+    }
+    else
+    {
+        return s.substr(mid, 1);
+    }
+    
+}
+
+int main()
+{
+    cout << solution("abcde") << endl;
+    cout << solution("qwer") << endl;
+}
