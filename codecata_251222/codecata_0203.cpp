fea260d7b1d597620b7d8d8119a6c7c5b5bfb86a
yunaj9910-alt <yunaj9910@gmail.com>
2026-02-03 09:25:21 +0900

    codecata_0203

---
+++ codecata_251222/codecata_0203.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    int answer = 0;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        answer += a[i] * b[i];
+
+    }
+    
+    return answer;
+}
+
+int main()
+{
+    vector<int> a1 = { 1, 2, 3, 4 };
+    vector<int> b1 = { -3, -1, 0, 2 };
+    // solution 함수를 호출하고 결과를 출력합니다.
+    cout << solution(a1, b1)<< endl;
+
+    
+    vector<int> a2 = { -1, 0, 1 };
+    vector<int> b2 = { 1, 0, -1 };
+    // solution 함수를 호출하고 결과를 출력합니다.
+    cout << solution(a2, b2)<< endl;
+
+    return 0;
+}
