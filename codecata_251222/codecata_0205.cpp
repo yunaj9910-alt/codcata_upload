4cabfaf60f9119fdd9fbd5aea514a5e516d00703
yunaj9910-alt <yunaj9910@gmail.com>
2026-02-05 09:23:08 +0900

    codecata_0205

---
+++ codecata_251222/codecata_0205.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm> // sort 사용
+#include <functional> // greater 사용
+
+using namespace std;
+
+string solution(string s) {
+    
+    sort(s.begin(), s.end(), greater<char>());
+    
+    return s;
+}
+
+int main()
+{
+    cout << solution("Zbcdefg") << endl;
+}
