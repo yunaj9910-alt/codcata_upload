5ffd07125716c43ec772006a8b563fbc7cf6df3f
yunaj9910-alt <yunaj9910@gmail.com>
2026-02-06 09:22:55 +0900

    codecata_0206

---
+++ codecata_251222/codecata_0206.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = 0;
+
+    for (int i = 1; i <= count; i++)
+    {
+        answer += (long long)price * i;
+    }
+    if (money >=answer)
+    {
+        return 0;
+    }
+    else 
+    {
+        return answer - money;
+    }
+}
+
+int main()
+{
+    cout << solution(3, 20, 4) << endl;
+}
